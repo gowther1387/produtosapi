e41a2967d1540d8a94b9a5fcec28489234fd3a25
frederyck nascimento <lohangcfc@gmail.com>
2025-06-24 08:52:13 -0300

    Initial commit

---
+++ produtosapi/src/main/java/Learn_Spring_boot/produtosapi/Controller/ProdutoController.java
@@ -0,0 +1,45 @@
+package Learn_Spring_boot.produtosapi.Controller;
+
+import Learn_Spring_boot.produtosapi.model.Produto;
+import Learn_Spring_boot.produtosapi.repository.ProdutoRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto){
+        System.out.println("Produto recebido: " + produto);
+
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+
+        produtoRepository.save(produto);
+        return produto;
+    }
+
+    @GetMapping("/{id}")
+    public Produto obterPorId(@PathVariable String id){
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletar(@PathVariable("id") String id){
+        produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable("id") String id, @RequestBody Produto produto){
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+}
---
+++ produtosapi/src/test/java/Learn_Spring_boot/produtosapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Learn_Spring_boot.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
