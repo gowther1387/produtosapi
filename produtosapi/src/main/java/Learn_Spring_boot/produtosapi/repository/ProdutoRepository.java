2520f2854371ae601c8868cbdbdb71a85d49fe6d
Frederyck_Lohan <52873156+gowther1387@users.noreply.github.com>
2025-06-24 09:25:21 -0300

    Update ProdutoRepository.java

--- produtosapi/src/main/java/Learn_Spring_boot/produtosapi/repository/ProdutoRepository.java
+++ produtosapi/src/main/java/Learn_Spring_boot/produtosapi/repository/ProdutoRepository.java
@@ -3,4 +3,6 @@ package Learn_Spring_boot.produtosapi.repository;
 import Learn_Spring_boot.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProdutoRepository extends JpaRepository <Produto, String>{ }
+public interface ProdutoRepository extends JpaRepository <Produto, String>{
+      List<Produto> findByNome(String nome);
+}
